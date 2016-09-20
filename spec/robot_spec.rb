dff45ccc331fe04968ccb608e0f0987253eae6a4
Jason Low <lowjiashen@gmail.com>
2016-09-21 01:59:57 +0800

    changed specs

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,47 +1,34 @@
-require_relative '../src/robot.rb'
+require_relative '../src/robot'
+require_relative '../src/map'
 
 describe Robot do
-  before do
-    @robot = Robot.new(0,0,"EAST")
-  end
+  let(:instance) { Robot.new(Map.new) }
+
   describe '.new' do
     it 'Creates a new Robot object' do
-      expect(@robot).to_not eq nil
+      expect(instance).to be_an_instance_of(Robot)
     end
   end
 
-  describe 'direction' do
-    it 'Should be the direction that I created the Robot with' do
-      expect(@robot.direction).to eq 'EAST'
+  describe '#place' do
+    context 'given a valid coordinate' do
+      before { instance.place(0, 0, 'NORTH') }
+
+      it 'will be placed on the map successfully' do
+        expect(instance.instance_variable_get(:@x)).to eq 0
+        expect(instance.instance_variable_get(:@y)).to eq 0
+        expect(instance.instance_variable_get(:@direction)).to eq 'NORTH'
+      end
+    end
+
+    context 'given a bad coordinate' do
+      before { instance.place(5, 5, 'RUBBISH') }
+
+      it 'will not place itself on the map' do
+        expect(instance.instance_variable_get(:@x)).to eq nil
+        expect(instance.instance_variable_get(:@y)).to eq nil
+        expect(instance.instance_variable_get(:@direction)).to eq nil
+      end
     end
   end
-
-  before do
-    @robot.left
-  end
-
-  context 'after moving left, robot direction should be south' do
-    it 'Should be able to turn 90 degrees left of current direction' do
-      expect(@robot.direction).to eq 'SOUTH'
-    end
-  end
-end
-  describe 'left' do
-
-  before do
-    @robot.right
-  end
-
-  describe 'right' do
-    it 'Should be able to turn 90 degrees right of current direction' do
-      expect(@robot.direction).to eq 'EAST'
-    end
-  end
-
-  # describe 'report' do
-  #   it 'Should return the current location and direction of robot' do
-  #     expect(robot.report).to eq "OUTPUT: #{robot.x}, #{robot.y}, #{robot.direction}"
-  #   end
-  # end
-
 end
